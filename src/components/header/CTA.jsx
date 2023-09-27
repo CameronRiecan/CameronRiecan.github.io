973fdc5d8fd8d6fdf2f18e22e5f777ac2312e72c
Cameron <124470971+CameronRiecan@users.noreply.github.com>
2023-09-27 14:01:12 -0700

    add picture of me and to call of action component, may change picture later

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import CV from '../../assets/'
+import CV from '../../assets/Cameron_Riecan_CV.pdf'
 
 const CTA = () => {
   return (
     <div className='cta'>
-       <a href=''>Download CV</a>
-       <a href='#contact'>Contact me!</a>
+       <a href={CV} download className='btn'>Download CV</a>
+       <a href='#contact' className='btn btn-primary'>Contact me!</a>
     </div>
   )
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.png'
 
 const Header = () => {
   return (
@@ -8,6 +10,11 @@ const Header = () => {
         <h5>Hello I'm</h5>
         <h1>Cameron Riecan</h1>
         <h5 className="text-light">Frontend Developer</h5>
+        <CTA />
+
+        <div>
+          <img src={ME} alt='Cameron Riecan Picture' />
+        </div>
       </div>
     </header>
   )
