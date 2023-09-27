6ff33b8b5dc4060e8441bdbc2ff6eed1591b5174
Cameron <124470971+CameronRiecan@users.noreply.github.com>
2023-09-27 14:50:28 -0700

    fix image to fix without primary color background

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -6,9 +6,9 @@ import {BsInstagram} from 'react-icons/bs'
 const HeaderSocials = () => {
   return (
     <div className='header_socials'>
-        <a href='https://www.linkedin.com/in/cameron-riecan/' target='_blank'><BsLinkedin /> Linkedin</a>
-        <a href='https://github.com/CameronRiecan' target='_blank'><FaGithub /> Github</a>
-        <a href='https://www.instagram.com/cameronriecan/' target='_blank'><BsInstagram /> Instagram</a>
+        <a href='https://www.linkedin.com/in/cameron-riecan/' target='_blank'><BsLinkedin /> Linkedin </a>
+        <a href='https://github.com/CameronRiecan' target='_blank'><FaGithub /> Github </a>
+        <a href='https://www.instagram.com/cameronriecan/' target='_blank'><BsInstagram /> Instagram </a>
     </div>
   )
 }
