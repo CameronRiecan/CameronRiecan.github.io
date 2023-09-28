c4ce99bbb1356d2352a0099e8a3360b2700e9b49
Cameron <124470971+CameronRiecan@users.noreply.github.com>
2023-09-27 17:39:51 -0700

    add content for about me component and icons and include a paragraph to be filled in later with more content

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/me-about.jpg'
+import {FaAward} from 'react-icons/fa'
+import {BiBook} from 'react-icons/bi'
+import {VscFolderLibrary} from 'react-icons/vsc'
 
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5>Get to Know</h5>
+      <h2>About me</h2>
+
+      <div className="container about_container">
+        <div className="about_me">
+          <img src={ME} alt='About Image' />
+        </div>
+        <div className="about_content">
+          <article className='about_card'>
+            <FaAward className='about_icon'/>
+            <h5>Experience</h5>
+            <small>2+ Years</small>
+          </article>
+          <article className='about_card'>
+            <BiBook className='about_icon'/>
+            <h5>Education</h5>
+            <small>Bachelors</small>
+            <small>Juris Doctorate</small>
+            <small>Meta Frontend Developer Certificate</small>
+          </article>
+          <article className='about_card'>
+            <VscFolderLibrary className='about_icon'/>
+            <h5>Projects</h5>
+            <small>React, Vanilla JS, and more!</small>
+          </article>
+        </div>
+
+        <p>
+          Lorem ipsum...........
+        </p>
+
+        <a href='#contact' className='btn btn-primary'>Let's Talk</a>
+      </div>
+    </section>
   )
 }
 
