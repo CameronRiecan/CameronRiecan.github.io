a5bdaba9842d9b3f8c2e98ec200ed4881e1cc6ae
Cameron <124470971+CameronRiecan@users.noreply.github.com>
2023-09-28 16:00:04 -0700

    install email js and link it up to the form to send me an email through the form, and make sure it resets form when i submit

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,8 +3,25 @@ import './contact.css'
 import {MdOutlineEmail} from 'react-icons/md'
 import {BsLinkedin} from 'react-icons/bs'
 import {BsInstagram} from 'react-icons/bs'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com'
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_fd17cgp', 'template_s4fx7um', form.current, 'yqxhU25qfuV7-9MSo')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+
+    e.target.reset();
+  };
+
   return (
     <section id='contact'>
       <h5>Get In Touch</h5>
@@ -33,7 +50,7 @@ const Contact = () => {
         </div>
         {/* End of Contact Options */}
 
-        <form action=''>
+        <form ref={form} onSubmit={sendEmail}>
           <input type="text" name='name' placeholder='Your Full Name' required />
           <input type='email' name='email' placeholder='Your Email' required />
           <textarea name='message' rows='7' placeholder='Your Message' required></textarea>
