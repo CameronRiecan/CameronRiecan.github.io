d78bf1aa3061d0f22b4535f6151b7d9aaab512d6
Cameron <124470971+CameronRiecan@users.noreply.github.com>
2023-09-28 15:28:07 -0700

    add futher styling of the form and contact options

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -13,19 +13,19 @@ const Contact = () => {
       <div className='container contact_container'>
         <div className='contact_options'>
           <article className='contact_option'>
-            <MdOutlineEmail />
+            <MdOutlineEmail className='content_option-icon' />
             <h4>Email</h4>
             <h5>camriecan@gmail.com</h5>
             <a href='mailto:camriecan@gmail.com' target='_blank'>Send a Message</a>
           </article>
           <article className='contact_option'>
-            <BsLinkedin />
+            <BsLinkedin className='content_option-icon' />
             <h4>Messenger</h4>
             <h5>Linkedin</h5>
             <a href='https://www.linkedin.com/in/cameron-riecan' target='_blank'>Send a Message</a>
           </article>
           <article className='contact_option'>
-            <BsInstagram />
+            <BsInstagram className='content_option-icon' />
             <h4>DM</h4>
             <h5>Instagram</h5>
             <a href='https://ig.me/m/cameronriecan' target='_blank'>Send a Message</a>
