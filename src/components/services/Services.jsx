8b213ab4a83e1d3ad9031146b46728c8cc9fe005
Cameron <124470971+CameronRiecan@users.noreply.github.com>
2023-10-03 11:06:33 -0700

    fix style error, caused by row container class conflict in jsx

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -8,7 +8,7 @@ const Services = () => {
       <h5>What I Offer</h5>
       <h2>Services</h2>
 
-      <div className='container services_container row-container'>
+      <div className='container services_container'>
         <article className='service'>
           <div className='service_head'>
             <h3>UI/UX Design</h3>
