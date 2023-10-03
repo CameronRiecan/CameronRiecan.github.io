cb892ecf95803e4a71c0cd41abfa4074b21fddaf
Cameron <124470971+CameronRiecan@users.noreply.github.com>
2023-10-02 22:52:45 -0700

    add more rel noreferrer to links, this time in header social component

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -6,9 +6,9 @@ import {BsInstagram} from 'react-icons/bs'
 const HeaderSocials = () => {
   return (
     <div className='header_socials'>
-        <a href='https://www.linkedin.com/in/cameron-riecan/' target='_blank'><BsLinkedin /><h5>Linkedin</h5></a>
-        <a href='https://github.com/CameronRiecan' target='_blank'><FaGithub /><h5>Github</h5></a>
-        <a href='https://www.instagram.com/cameronriecan/' target='_blank'><BsInstagram /><h5>Instagram</h5></a>
+        <a href='https://www.linkedin.com/in/cameron-riecan/' target='_blank' rel='noreferrer'><BsLinkedin /><h5>Linkedin</h5></a>
+        <a href='https://github.com/CameronRiecan' target='_blank' rel='noreferrer'><FaGithub /><h5>Github</h5></a>
+        <a href='https://www.instagram.com/cameronriecan/' target='_blank' rel='noreferrer'><BsInstagram /><h5>Instagram</h5></a>
     </div>
   )
 }
