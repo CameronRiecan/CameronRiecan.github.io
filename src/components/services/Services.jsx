fe732afaa9a8db6f301153abbcbefa40739ba8a3
Cameron <124470971+CameronRiecan@users.noreply.github.com>
2023-10-03 09:10:39 -0700

    add and revise services content

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -52,31 +52,31 @@ const Services = () => {
           <ul className='service_list'>
             <li>
               <BiCheck className='service_list-icon'/>
-              <p>Frontend Excellence: My frontend skills involve HTML5, CSS3, JavaScript, and frameworks like React.js for dynamic and interactive user interfaces.</p>
+              <p>Frontend Excellence. HTML5, CSS3, JavaScript, and React.js for dynamic and interactive user interfaces.</p>
             </li>
             <li>
               <BiCheck className='service_list-icon'/>
-              <p>Version Control: I use Git for version control, enabling seamless collaboration and code management.</p>
+              <p>Version Control. Git for version control, enabling seamless collaboration and code management.</p>
             </li>
             <li>
               <BiCheck className='service_list-icon'/>
-              <p>Responsive Web Design: I focus on responsive web design to ensure websites perform flawlessly on all devices.</p>
+              <p>Responsive Web Design. Responsive web design to ensure websites perform flawlessly on all devices.</p>
             </li>
             <li>
               <BiCheck className='service_list-icon'/>
-              <p>Web Performance Optimization: My goal is to create websites that load quickly and efficiently, enhancing the user experience.</p>
+              <p>Web Performance Optimization. Create websites that load quickly and efficiently, enhancing the user experience.</p>
             </li>
             <li>
               <BiCheck className='service_list-icon'/>
-              <p>Cross-Browser Compatibility: I test and optimize websites for cross-browser compatibility to reach a broader audience.</p>
+              <p>Cross-Browser Compatibility. Test and optimize websites for cross-browser compatibility to reach a broader audience.</p>
             </li>
             <li>
               <BiCheck className='service_list-icon'/>
-              <p>Performance Analysis with Browser Tools: I utilize browser developer tools to analyze and optimize your website's performance, identifying areas for improvement and enhancing the user experience.</p>
+              <p>Performance Analysis with Browser Tools. Analyze and optimize website's performance, and identifying areas for improvement.</p>
             </li>
             <li>
               <BiCheck className='service_list-icon'/>
-              <p>Continuous Learning: Staying updated with the latest web development trends and technologies is essential to my practice. I continually seek opportunities to learn and implement new techniques.</p>
+              <p>Continuous Learning. Staying updated with the latest web development trends and technologies.</p>
             </li>
           </ul>
         </article>
@@ -91,27 +91,27 @@ const Services = () => {
           <ul className='service_list'>
             <li>
               <BiCheck className='service_list-icon'/>
-              <p>Content Strategy: Crafting a content strategy, I align content with your goals, target audience, and brand identity.</p>
+              <p>Content Strategy. Align content with goals, target audience, and brand identity.</p>
             </li>
             <li>
               <BiCheck className='service_list-icon'/>
-              <p>Engaging Copy: With a Juris Doctorate degree and extensive legal writing experience, I bring a unique perspective to content creation. My writing skills extend to creating engaging and persuasive copy that resonates with your audience.</p>
+              <p>Engaging Copy. Juris Doctorate degree and extensive legal writing experience brings unique perspective to content creation.</p>
             </li>
             <li>
               <BiCheck className='service_list-icon'/>
-              <p>Multimedia Content: I can produce multimedia content, including images, videos, and infographics, to enrich your website.</p>
+              <p>Multimedia Content. Images, videos, etc., to enrich your website.</p>
             </li>
             <li>
               <BiCheck className='service_list-icon'/>
-              <p>SEO Optimization: I optimize content for search engines, improving your website's visibility.</p>
+              <p>SEO Optimization. Optimize content for search engines, improving website's visibility.</p>
             </li>
             <li>
               <BiCheck className='service_list-icon'/>
-              <p>Content Management: ensuring that your website stays fresh and up-to-date.</p>
+              <p>Content Management. Ensuring website stays fresh and up-to-date.</p>
             </li>
             <li>
               <BiCheck className='service_list-icon'/>
-              <p>AI-Powered Content: I leverage cutting-edge AI technologies, including chatbots and chatGPT, to create dynamic and personalized content experiences for your audience.</p>
+              <p>AI-Powered Content. Leverage cutting-edge AI technologies like chatGPT, to create dynamic and personalized content experiences</p>
             </li>
           </ul>
         </article>
