548ac2178f0733ce96d2b5cb279c8bc672f9ba30
Cameron <124470971+CameronRiecan@users.noreply.github.com>
2023-10-02 22:49:44 -0700

    add javascript in footer so the logo and home link go to the very top as id on body or root dont work as header has margin top, so to go to very top had to create function

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,12 +5,19 @@ import {FaGithub} from 'react-icons/fa'
 import {BsInstagram} from 'react-icons/bs'
 
 const Footer = () => {
+  const scrollToTop = () => {
+    window.scrollTo({
+      top:0,
+      behavior: 'smooth',
+    });
+  };
+
   return (
     <footer>
-      <a href='#' className='footer_logo'>Cameron Riecan</a>
+      <a href='#top' onClick={scrollToTop} className='footer_logo'>Cameron Riecan</a>
 
       <ul className='permalinks'>
-        <li><a href='#'>Home</a></li>
+        <li><a href='#top' onClick={scrollToTop}>Home</a></li>
         <li><a href='#about'>About</a></li>
         <li><a href='#experiences'>Experience</a></li>
         <li><a href='#services'>Services</a></li>
@@ -20,9 +27,9 @@ const Footer = () => {
       </ul>
 
       <div className='footer_socials'>
-        <a href='https://www.linkedin.com/in/cameron-riecan/' target='_blank'><BsLinkedin /></a>
-        <a href='https://github.com/CameronRiecan' target='_blank'><FaGithub /></a>
-        <a href='https://www.instagram.com/cameronriecan/' target='_blank'><BsInstagram /></a>
+        <a href='https://www.linkedin.com/in/cameron-riecan/' target='_blank' rel='noreferrer'><BsLinkedin /></a>
+        <a href='https://github.com/CameronRiecan' target='_blank' rel='noreferrer'><FaGithub /></a>
+        <a href='https://www.instagram.com/cameronriecan/' target='_blank' rel='noreferrer'><BsInstagram /></a>
       </div>
 
       <div className="footer_copyright">
