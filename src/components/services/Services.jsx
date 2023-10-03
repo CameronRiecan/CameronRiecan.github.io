499e5a8fd230663913908eba472afbe927c2281f
Cameron <124470971+CameronRiecan@users.noreply.github.com>
2023-10-03 09:18:35 -0700

    add bold tag to titles of each paragraph on services card lists

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -17,27 +17,27 @@ const Services = () => {
           <ul className='service_list'>
             <li>
               <BiCheck className='service_list-icon'/>
-              <p>User-Centered Design. Every element is crafted with the end-user in mind.</p>
+              <p><b>User-Centered Design.</b> Every element is crafted with the end-user in mind.</p>
             </li>
             <li>
               <BiCheck className='service_list-icon'/>
-              <p>Wireframing and Prototyping. Visualize the user journey and iterate on design concepts.</p>
+              <p><b>Wireframing and Prototyping.</b> Visualize the user journey and iterate on design concepts.</p>
             </li>
             <li>
               <BiCheck className='service_list-icon'/>
-              <p>Responsive Design. Prioritize responsive design principles for optimal experience across various devices.</p>
+              <p><b>Responsive Design.</b> Prioritize responsive design principles for optimal experience across various devices.</p>
             </li>
             <li>
               <BiCheck className='service_list-icon'/>
-              <p>Accessibility. Ensuring inclusivity, implement accessibility features for a wider audience.</p>
+              <p><b>Accessibility.</b> Ensuring inclusivity, implement accessibility features for a wider audience.</p>
             </li>
             <li>
               <BiCheck className='service_list-icon'/>
-              <p>Mobile-First Design. Provides an excellent user experience on smartphones and tablets.</p>
+              <p><b>Mobile-First Design.</b> Provides an excellent user experience on smartphones and tablets.</p>
             </li>
             <li>
               <BiCheck className='service_list-icon'/>
-              <p>Visual Design. Visually appealing and intuitive interfaces that captivate users.</p>
+              <p><b>Visual Design.</b> Visually appealing and intuitive interfaces that captivate users.</p>
             </li>
           </ul>
         </article>
@@ -52,31 +52,31 @@ const Services = () => {
           <ul className='service_list'>
             <li>
               <BiCheck className='service_list-icon'/>
-              <p>Frontend Excellence. HTML5, CSS3, JavaScript, and React.js for dynamic and interactive user interfaces.</p>
+              <p><b>Frontend Excellence.</b> HTML5, CSS3, JavaScript, and React.js for dynamic and interactive user interfaces.</p>
             </li>
             <li>
               <BiCheck className='service_list-icon'/>
-              <p>Version Control. Git for version control, enabling seamless collaboration and code management.</p>
+              <p><b>Version Control.</b> Git for version control, enabling seamless collaboration and code management.</p>
             </li>
             <li>
               <BiCheck className='service_list-icon'/>
-              <p>Responsive Web Design. Responsive web design to ensure websites perform flawlessly on all devices.</p>
+              <p><b>Responsive Web Design.</b> Responsive web design to ensure websites perform flawlessly on all devices.</p>
             </li>
             <li>
               <BiCheck className='service_list-icon'/>
-              <p>Web Performance Optimization. Create websites that load quickly and efficiently, enhancing the user experience.</p>
+              <p><b>Web Performance Optimization.</b> Create websites that load quickly and efficiently, enhancing the user experience.</p>
             </li>
             <li>
               <BiCheck className='service_list-icon'/>
-              <p>Cross-Browser Compatibility. Test and optimize websites for cross-browser compatibility to reach a broader audience.</p>
+              <p><b>Cross-Browser Compatibility.</b> Test and optimize websites for cross-browser compatibility to reach a broader audience.</p>
             </li>
             <li>
               <BiCheck className='service_list-icon'/>
-              <p>Performance Analysis with Browser Tools. Analyze and optimize website's performance, and identifying areas for improvement.</p>
+              <p><b>Performance Analysis with Browser Tools.</b> Analyze and optimize website's performance, and identifying areas for improvement.</p>
             </li>
             <li>
               <BiCheck className='service_list-icon'/>
-              <p>Continuous Learning. Staying updated with the latest web development trends and technologies.</p>
+              <p><b>Continuous Learning.</b> Staying updated with the latest web development trends and technologies.</p>
             </li>
           </ul>
         </article>
@@ -91,27 +91,27 @@ const Services = () => {
           <ul className='service_list'>
             <li>
               <BiCheck className='service_list-icon'/>
-              <p>Content Strategy. Align content with goals, target audience, and brand identity.</p>
+              <p><b>Content Strategy.</b> Align content with goals, target audience, and brand identity.</p>
             </li>
             <li>
               <BiCheck className='service_list-icon'/>
-              <p>Engaging Copy. Juris Doctorate degree and extensive legal writing experience brings unique perspective to content creation.</p>
+              <p><b>Engaging Copy.</b> Juris Doctorate degree and extensive legal writing experience brings unique perspective to content creation.</p>
             </li>
             <li>
               <BiCheck className='service_list-icon'/>
-              <p>Multimedia Content. Images, videos, etc., to enrich your website.</p>
+              <p><b>Multimedia Content.</b> Images, videos, etc., to enrich your website.</p>
             </li>
             <li>
               <BiCheck className='service_list-icon'/>
-              <p>SEO Optimization. Optimize content for search engines, improving website's visibility.</p>
+              <p><b>SEO Optimization.</b> Optimize content for search engines, improving website's visibility.</p>
             </li>
             <li>
               <BiCheck className='service_list-icon'/>
-              <p>Content Management. Ensuring website stays fresh and up-to-date.</p>
+              <p><b>Content Management.</b> Ensuring website stays fresh and up-to-date.</p>
             </li>
             <li>
               <BiCheck className='service_list-icon'/>
-              <p>AI-Powered Content. Leverage cutting-edge AI technologies like chatGPT, to create dynamic and personalized content experiences</p>
+              <p><b>AI-Powered Content.</b> Leverage cutting-edge AI technologies like chatGPT, to create dynamic and personalized content experiences</p>
             </li>
           </ul>
         </article>
