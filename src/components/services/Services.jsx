65bb116d7984b3159328922abc9dc4cf3309f550
Cameron <124470971+CameronRiecan@users.noreply.github.com>
2023-10-04 12:21:05 -0700

    add accessibility, add aria hidden true to all icons so its known to be decorative in services component

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -16,27 +16,27 @@ const Services = () => {
 
           <ul className='service_list'>
             <li>
-              <BiCheck className='service_list-icon'/>
+              <BiCheck className='service_list-icon' aria-hidden='true' />
               <p><b>User-Centered Design.</b> Every element is crafted with the end-user in mind.</p>
             </li>
             <li>
-              <BiCheck className='service_list-icon'/>
+              <BiCheck className='service_list-icon' aria-hidden='true' />
               <p><b>Wireframing and Prototyping.</b> Visualize the user journey and iterate on design concepts.</p>
             </li>
             <li>
-              <BiCheck className='service_list-icon'/>
+              <BiCheck className='service_list-icon' aria-hidden='true' />
               <p><b>Responsive Design.</b> Prioritize responsive design principles for optimal experience across various devices.</p>
             </li>
             <li>
-              <BiCheck className='service_list-icon'/>
+              <BiCheck className='service_list-icon' aria-hidden='true' />
               <p><b>Accessibility.</b> Ensuring inclusivity, implement accessibility features for a wider audience.</p>
             </li>
             <li>
-              <BiCheck className='service_list-icon'/>
+              <BiCheck className='service_list-icon' aria-hidden='true' />
               <p><b>Mobile-First Design.</b> Provides an excellent user experience on smartphones and tablets.</p>
             </li>
             <li>
-              <BiCheck className='service_list-icon'/>
+              <BiCheck className='service_list-icon' aria-hidden='true' />
               <p><b>Visual Design.</b> Visually appealing and intuitive interfaces that captivate users.</p>
             </li>
           </ul>
@@ -51,31 +51,31 @@ const Services = () => {
 
           <ul className='service_list'>
             <li>
-              <BiCheck className='service_list-icon'/>
+              <BiCheck className='service_list-icon' aria-hidden='true' />
               <p><b>Frontend Excellence.</b> HTML5, CSS3, JavaScript, and React.js for dynamic and interactive user interfaces.</p>
             </li>
             <li>
-              <BiCheck className='service_list-icon'/>
+              <BiCheck className='service_list-icon' aria-hidden='true' />
               <p><b>Version Control.</b> Git for version control, enabling seamless collaboration and code management.</p>
             </li>
             <li>
-              <BiCheck className='service_list-icon'/>
+              <BiCheck className='service_list-icon' aria-hidden='true' />
               <p><b>Responsive Web Design.</b> Responsive web design to ensure websites perform flawlessly on all devices.</p>
             </li>
             <li>
-              <BiCheck className='service_list-icon'/>
+              <BiCheck className='service_list-icon' aria-hidden='true' />
               <p><b>Web Performance Optimization.</b> Create websites that load quickly and efficiently, enhancing the user experience.</p>
             </li>
             <li>
-              <BiCheck className='service_list-icon'/>
+              <BiCheck className='service_list-icon' aria-hidden='true' />
               <p><b>Cross-Browser Compatibility.</b> Test and optimize websites for cross-browser compatibility to reach a broader audience.</p>
             </li>
             <li>
-              <BiCheck className='service_list-icon'/>
+              <BiCheck className='service_list-icon' aria-hidden='true' />
               <p><b>Performance Analysis with Browser Tools.</b> Analyze and optimize website's performance, and identifying areas for improvement.</p>
             </li>
             <li>
-              <BiCheck className='service_list-icon'/>
+              <BiCheck className='service_list-icon' aria-hidden='true' />
               <p><b>Continuous Learning.</b> Staying updated with the latest web development trends and technologies.</p>
             </li>
           </ul>
@@ -90,27 +90,27 @@ const Services = () => {
 
           <ul className='service_list'>
             <li>
-              <BiCheck className='service_list-icon'/>
+              <BiCheck className='service_list-icon' aria-hidden='true' />
               <p><b>Content Strategy.</b> Align content with goals, target audience, and brand identity.</p>
             </li>
             <li>
-              <BiCheck className='service_list-icon'/>
+              <BiCheck className='service_list-icon' aria-hidden='true' />
               <p><b>Engaging Copy.</b> Juris Doctorate degree and extensive legal writing experience brings unique perspective.</p>
             </li>
             <li>
-              <BiCheck className='service_list-icon'/>
+              <BiCheck className='service_list-icon' aria-hidden='true' />
               <p><b>Multimedia Content.</b> Images, videos, etc., to enrich your website.</p>
             </li>
             <li>
-              <BiCheck className='service_list-icon'/>
+              <BiCheck className='service_list-icon' aria-hidden='true' />
               <p><b>SEO Optimization.</b> Optimize content for search engines, improving website's visibility.</p>
             </li>
             <li>
-              <BiCheck className='service_list-icon'/>
+              <BiCheck className='service_list-icon' aria-hidden='true' />
               <p><b>Content Management.</b> Ensuring website stays fresh and up-to-date.</p>
             </li>
             <li>
-              <BiCheck className='service_list-icon'/>
+              <BiCheck className='service_list-icon' aria-hidden='true' />
               <p><b>AI-Powered Content.</b> Leverage cutting-edge AI technologies like chatGPT, to create dynamic and personalized content.</p>
             </li>
           </ul>
