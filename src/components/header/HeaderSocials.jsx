c3cdad70b9b1813ed6a7e13ee1862402c34cf238
Cameron <124470971+CameronRiecan@users.noreply.github.com>
2023-10-04 11:16:08 -0700

    add accessibility, aria labels and class for sr only and reformat of code for easier readability

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -5,10 +5,34 @@ import {BsInstagram} from 'react-icons/bs'
 
 const HeaderSocials = () => {
   return (
-    <div className='header_socials'>
-        <a href='https://www.linkedin.com/in/cameron-riecan/' target='_blank' rel='noreferrer'><BsLinkedin /><h5>Linkedin</h5></a>
-        <a href='https://github.com/CameronRiecan' target='_blank' rel='noreferrer'><FaGithub /><h5>Github</h5></a>
-        <a href='https://www.instagram.com/cameronriecan/' target='_blank' rel='noreferrer'><BsInstagram /><h5>Instagram</h5></a>
+    <div className='header_socials' aria-label='Social Media Links'>
+        <a
+          href='https://www.linkedin.com/in/cameron-riecan/'
+          target='_blank'
+          rel='noopener noreferrer'
+          aria-label='LinkedIn'
+        >
+          <BsLinkedin />
+          <h5 className='sr-only'>Linkedin</h5>
+        </a>
+        <a
+          href='https://github.com/CameronRiecan'
+          target='_blank'
+          rel='noopener noreferrer'
+          aria-label='Github'
+        >
+          <FaGithub />
+          <h5 className='sr-only'>Github</h5>
+        </a>
+        <a
+          href='https://www.instagram.com/cameronriecan/'
+          target='_blank'
+          rel='noopener noreferrer'
+          aria-label='Instagram'
+        >
+          <BsInstagram />
+          <h5 className='sr-only'>Instagram</h5>
+        </a>
     </div>
   )
 }
