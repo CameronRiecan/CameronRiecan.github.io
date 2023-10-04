2dec62dde86d9493dc6b212f698812ce4acf82ee
Cameron <124470971+CameronRiecan@users.noreply.github.com>
2023-10-04 12:15:14 -0700

    add accessibility to experience component jsx to indicate icons are decorative and should be ignored by assistive technologies and add aria labels for screen readers

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -13,52 +13,52 @@ const Experience = () => {
           <h3>Frontend Development</h3>
           <div className="experience_content">
             <article className='experience_details'>
-              <BsPatchCheckFill className='experience_details-icon' />
+              <BsPatchCheckFill className='experience_details-icon' aria-hidden='true' />
               <div>
                 <h4>HTML</h4>
-                <small className='text-light'>Experienced</small>
+                <small className='text-light' aria-label="Experience Level: Experienced">Experienced</small>
               </div>
             </article>
             <article className='experience_details'>
-              <BsPatchCheckFill className='experience_details-icon' />
+              <BsPatchCheckFill className='experience_details-icon' aria-hidden='true' />
               <div>
                 <h4>CSS</h4>
-                <small className='text-light'>Intermediate</small>
+                <small className='text-light' aria-label="Experience Level: Intermediate">Intermediate</small>
               </div>
             </article>
             <article className='experience_details'>
-              <BsPatchCheckFill className='experience_details-icon' />
+              <BsPatchCheckFill className='experience_details-icon' aria-hidden='true' />
               <div>
                 <h4>Javascript</h4>
-                <small className='text-light'>Intermediate</small>
+                <small className='text-light' aria-label="Experience Level: Intermediate">Intermediate</small>
               </div>
             </article>
             <article className='experience_details'>
-              <BsPatchCheckFill className='experience_details-icon' />
+              <BsPatchCheckFill className='experience_details-icon' aria-hidden='true' />
               <div>
                 <h4>Git</h4>
-                <small className='text-light'>Experienced</small>
+                <small className='text-light' aria-label="Experience Level: Experienced">Experienced</small>
               </div>
             </article>
             <article className='experience_details'>
-              <BsPatchCheckFill className='experience_details-icon' />
+              <BsPatchCheckFill className='experience_details-icon' aria-hidden='true' />
               <div>
                 <h4>Bootstrap</h4>
-                <small className='text-light'>Experienced</small>
+                <small className='text-light' aria-label="Experience Level: Experienced">Experienced</small>
               </div>
             </article>
             <article className='experience_details'>
-              <BsPatchCheckFill className='experience_details-icon' />
+              <BsPatchCheckFill className='experience_details-icon' aria-hidden='true' />
               <div>
                 <h4>React</h4>
-                <small className='text-light'>Intermediate</small>
+                <small className='text-light' aria-label="Experience Level: Intermediate">Intermediate</small>
               </div>
             </article>
             <article className='experience_details'>
-              <BsPatchCheckFill className='experience_details-icon' />
+              <BsPatchCheckFill className='experience_details-icon' aria-hidden='true' />
               <div>
                 <h4>Command Line</h4>
-                <small className='text-light'>Beginner/Intermediate</small>
+                <small className='text-light' aria-label="Experience Level: Beginner/Intermediate">Beginner/Intermediate</small>
               </div>
             </article>
           </div>
@@ -70,45 +70,45 @@ const Experience = () => {
           <h3>Other Tech</h3>
           <div className='experience_content'>
             <article className='experience_details'>
-              <BsPatchCheckFill className='experience_details-icon' />
+              <BsPatchCheckFill className='experience_details-icon' aria-hidden='true' />
               <div>
                 <h4>Github</h4>
-                <small className='text-light'>Experienced</small>
+                <small className='text-light' aria-label="Experience Level: Experienced">Experienced</small>
               </div>
             </article>
             <article className='experience_details'>
-              <BsPatchCheckFill className='experience_details-icon' />
+              <BsPatchCheckFill className='experience_details-icon' aria-hidden='true' />
               <div>
                 <h4>PC</h4>
-                <small className='text-light'>Experienced</small>
+                <small className='text-light' aria-label="Experience Level: Experienced">Experienced</small>
               </div>
             </article>
             <article className='experience_details'>
-              <BsPatchCheckFill className='experience_details-icon' />
+              <BsPatchCheckFill className='experience_details-icon' aria-hidden='true' />
               <div>
                 <h4>Mac</h4>
-                <small className='text-light'>Experienced</small>
+                <small className='text-light' aria-label="Experience Level: Experienced">Experienced</small>
               </div>
             </article>
             <article className='experience_details'>
-              <BsPatchCheckFill className='experience_details-icon' />
+              <BsPatchCheckFill className='experience_details-icon' aria-hidden='true' />
               <div>
                 <h4>Linux</h4>
-                <small className='text-light'>Intermediate</small>
+                <small className='text-light' aria-label="Experience Level: Intermediate">Intermediate</small>
               </div>
             </article>
             <article className='experience_details'>
-              <BsPatchCheckFill className='experience_details-icon' />
+              <BsPatchCheckFill className='experience_details-icon' aria-hidden='true' />
               <div>
                 <h4>VirtualBox</h4>
-                <small className='text-light'>Intermediate</small>
+                <small className='text-light' aria-label="Experience Level: Intermediate">Intermediate</small>
               </div>
             </article>
             <article className='experience_details'>
-              <BsPatchCheckFill className='experience_details-icon' />
+              <BsPatchCheckFill className='experience_details-icon' aria-hidden='true' />
               <div>
                 <h4>Figma</h4>
-                <small className='text-light'>Beginner/Intermediate</small>
+                <small className='text-light' aria-label="Experience Level: Beginner/Intermediate">Beginner/Intermediate</small>
               </div>
             </article>
           </div>
