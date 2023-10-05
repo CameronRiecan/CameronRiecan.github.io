571abd15f29ccba15d86445a1061fdb2d065aaa5
Cameron <124470971+CameronRiecan@users.noreply.github.com>
2023-10-04 17:04:17 -0700

    add javascript to index.js to disable the scroll restoration to prevent page from scrolling to previous position when the page is refreshed, so it goes to top of page instead

--- src/index.js
+++ src/index.js
@@ -3,4 +3,9 @@ import ReactDOM from 'react-dom'
 import App from './App.jsx'
 import './index.css'
 
+// Disable scroll restoration to prevent page from scrolling to previous position on refresh
+if ('scrollRestoration' in window.history) {
+    window.history.scrollRestoration = 'manual';
+  }
+
 ReactDOM.render(<App/>, document.querySelector("#root"))
